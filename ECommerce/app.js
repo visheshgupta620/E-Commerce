80cfa9bd24ee7080ffb8a7f50c9295af4a1611cc
Vishesh Gupta <visheshgupta841@gmail.com>
2023-08-13 00:55:01 +0530

    comments added

--- ECommerce/app.js
+++ ECommerce/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
-const methodOverride = require('method-override');
-const session = require('express-session');
-const flash = require('connect-flash');
+const path = require('path');                                  //
+const mongoose = require('mongoose');                          //to connect express and mongoDB
+const ejsMate = require('ejs-mate');                           //to create and use layout (boilerplate)
+const methodOverride = require('method-override');             //to send patch and delete request
+const session = require('express-session');                    //connect-flash session hone pe hi work krta
+const flash = require('connect-flash');                        //flash krna info vagara ko uske liye jaise item added , deleted vagara pop-up krane
 
 mongoose.connect('mongodb://127.0.0.1:27017/e-com-db')
     .then(() => { console.log('e-com-db connected!!') })
@@ -20,14 +20,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
-app.use(session({                   //session kyunki flash tbhi work krta
+app.use(session({                           //session kyunki flash tbhi work krta   //github se ye code
     secret: 'weneedbettersecret',
     resave: false,
     saveUninitialized: true,
 }))
-app.use(flash());                   //session ke baad hi chlega ye
 
-app.use((req, res, next)=>{                //local use krna pdta success ko use krne ke liye
+app.use(flash());                          //session ke baad hi chlega ye
+
+app.use((req, res, next)=>{                //middleware  //local use krna pdta success ko access krne ke liye
     res.locals.success = req.flash('success');
     next();
 })
