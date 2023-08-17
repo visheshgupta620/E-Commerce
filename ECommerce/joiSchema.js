20069773ec537a0d00fdbe5f0b1f5c273f1680e2
Vishesh Gupta <visheshgupta841@gmail.com>
2023-08-17 15:23:50 +0530

    comments

--- ECommerce/joiSchema.js
+++ ECommerce/joiSchema.js
@@ -8,3 +8,11 @@ module.exports.productSchema = Joi.object({
 });
 
 module.exports.reviewSchema = Joi.object({});
+
+
+
+// hum frontend aur backend dono me validation dete for more security
+// to front end validation ke liye humne bootstrap se validation form uthaya jiski html aur js hai likhi humne
+// but front end ko bypass krna easy hota hai isliye humne backend validation ke liye joi ko use kra
+//  to like generally hum use krenge site to frontend validation hi dikhegi hume form me kuch galat bhara toh 
+//  but agar koi usko bypass kar jata hai to we have joi for it . thus , more Security .   (frontend bypass hone par bhi backend validation(joi) bacha lega)
